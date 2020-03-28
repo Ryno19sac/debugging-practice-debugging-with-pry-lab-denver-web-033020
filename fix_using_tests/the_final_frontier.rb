b481893184cb34313bfa562cce99326352a2ceb0
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-28 23:08:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,27 +1,22 @@
- def generate_star_date
-   (rand(100000) + 400000) / 10.0
- end
-
-
- def state_log(star_date)
-   "Captain's Log, star date #{star_date}."
- end
-
-
- def crew
-   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
- end
-
- def greet_crew (crew)
-   counter = 0 
-   if crew.length < counter do
-   crew.each {|crew_member| "Hello #{crew_member}."}
-   counter += 1
- end
- return crew
+def generate_star_date
+  (rand(100000) + 400000) / 10.0
 end
 
-def engage   date = generate_star_date
-   state_log(date)
-   greet_crew(crew)
- end
+
+def state_log(star_date)
+  "Captain's Log, star date #{star_date}."
+end
+
+def crew
+  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+end
+
+def greet_crew (crew)
+  crew.map {|crew_member| "Hello #{crew_member}."}
+end
+
+def engage
+  date = generate_star_date
+  state_log(date)
+  greet_crew(crew)
+end
