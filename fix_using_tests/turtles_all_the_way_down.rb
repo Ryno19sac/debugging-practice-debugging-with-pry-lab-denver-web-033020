e95e441e4370bda44f509b8882017130d5f60ad5
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-28 23:10:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -8,12 +8,10 @@ def turtles
     ]
 end
 
-def turtle_traits(turtles)
-  
-  turtles.map do |turtle, trait|
-    #binding.pry
-    turtles[:traits].map do |trait|
-      
+  def turtle_traits(turtles)
+  turtles.map do |turtle|
+    turtle[:traits].each do |trait|
+      trait
     end
   end
 end
